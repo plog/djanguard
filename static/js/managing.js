554214e1bf7bde4fda8114b7b75715366ab8ea35
Plog <plog@plog.net>
2024-10-18 10:57:05 +0800

    UI

--- static/js/managing.js
+++ static/js/managing.js
@@ -14,59 +14,6 @@ const majorTimezones = [
     'America/Sao_Paulo'
 ];
 
-function handleDeleteSensor(selector, confirmationMessage) {
-    document.querySelectorAll(selector).forEach(button => {
-        button.addEventListener('click', function() {
-            const id    = this.getAttribute('data-sensor-id');
-            if (confirm(confirmationMessage)) {
-                $.ajax({
-                    url: `/api/sensor/${id}/`,
-                    method: 'delete',
-                    headers: {'X-CSRFToken': Cookies.get('csrftoken')},         
-                    success: function () {
-                        window.location.reload();
-                    },
-                    error: function (error) {
-                        console.error(`Failed to ${method} the item.`, error);
-                        alert(`Cannot ${method} item`);
-                    }                               
-                })
-            }
-        });
-    });
-}
-
-
-function handleAction(selector, method, confirmationMessage) {
-    document.querySelectorAll(selector).forEach(button => {
-        button.addEventListener('click', function() {
-            const id    = this.getAttribute('data-sensor-id');
-            const model = 'sensor'
-            let verb = null;
-            if(method == 'delete'){
-                verb = method;
-            }else{
-                verb = 'GET';
-            }
-            if (confirm(confirmationMessage)) {
-                $.ajax({
-                    url: `/api/${model}/${method}/${id}/`,
-                    method: verb,
-                    headers: {'X-CSRFToken': Cookies.get('csrftoken')},         
-                    success: function () {
-                        alert(`Item ${id} deleted`);
-                        window.location.reload();
-                    },
-                    error: function (error) {
-                        console.error(`Failed to ${method} the item.`, error);
-                        alert(`Cannot ${method} item`);
-                    }                               
-                })
-            }
-        });
-    });
-}
-
 // Debounce function to prevent frequent fetch calls during typing
 function debounce(func, delay) {
     let timer;
@@ -130,45 +77,6 @@ function formatDate(dateString) {
     }
 }
 
-// Fetch sensor data from the server
-async function fetchBackend(searchTerm, model) {
-    try {
-        const searchTermValue = searchTerm.value.toLowerCase();
-        const response = await fetch('/api/sensor/', {method: 'GET',headers: { 'Content-Type': 'application/json', 'search': searchTermValue }});
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        const data = await response.json();
-        console.log(data)
-       displaySensors(data)
-    } catch (error) {
-        console.error('Error fetching sensor:', error);
-    }
-}
-
-function setupSearchInput(searchInput,model){
-    const reload      = document.getElementById('reload');
-    if(searchInput && reload){
-        searchInput.addEventListener('keyup', debounce(() => {
-            spin('block');
-            fetchBackend(searchInput,model);
-        }, 500));
-        reload.addEventListener('click', () => {
-            spin('block');
-            fetchBackend(searchInput,model);
-        });
-    }
-}
-
-// Function to dynamically insert session data into the table
-function displaySensors(sensors) {
-    const tableBody = document.getElementById('sensor-table').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';  // Clear existing rows
-    sensors.forEach(sensor => {
-        tableBody.insertAdjacentHTML('beforeend', createSensorRow(sensor));
-    });
-    handleAction('.sensor-delete-btn' , 'delete' , 'Delete this sensor? THIS ACTION CANNOT BE UNDONE.');
-    spin('none');
-}
-
 function onlyNumberKey(evt) {
     // Only ASCII character in that range allowed
     let ASCIICode = (evt.which) ? evt.which : evt.keyCode
@@ -208,17 +116,13 @@ function onlyAlphaNumericKey(evt) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // setupTimezoneSelector();
-    // const searchSensors = document.getElementById('search-sensor-input');
-    // if(searchSensors){
-    //     setupSearchInput(searchSensors,'sensor')
-    //     spin('block');
-    //     fetchBackend(searchSensors,'sensor');
-    // }
-
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+    setupTimezoneSelector();
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log('DARK')
+    }     
 
 })
