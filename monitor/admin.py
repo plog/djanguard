73de91bee89f26dbff1b85dacd99cae17d084cd8
Plog <plog@plog.net>
2024-10-19 17:30:53 +0800

    UI

--- monitor/admin.py
+++ monitor/admin.py
@@ -10,19 +10,19 @@ admin.site.index_title = "Welcome to Djanguard Monitoring Dashboard"
 @admin.register(Sensor)
 class SensorAdmin(admin.ModelAdmin):
     search_fields = ['name', 'url']
-    list_display = ['name', 'url', 'favico', 'frequency', 'get_actions_count']
+    list_display  = ['id', 'name', 'url', 'favico', 'frequency', 'get_actions_count']
 
 # Custom Admin for Action Model
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
     search_fields = ['action_name', 'action_type', 'sensor__name']
-    list_display = ['action_name', 'action_type', 'action_path', 'last_execution', 'sensor', 'assertion_type', 'expected_value', 'sequence']
+    list_display  = ['action_name', 'action_type', 'action_path', 'last_execution', 'sensor', 'assertion_type', 'expected_value', 'sequence']
 
 # Custom Admin for TestResult Model
 @admin.register(TestResult)
 class TestResultAdmin(admin.ModelAdmin):
     search_fields = ['action__action_name', 'test_type', 'expected_value', 'actual_value']
-    list_display = ['action', 'test_type', 'expected_value', 'actual_value', 'timestamp']
+    list_display  = ['action', 'test_type', 'expected_value', 'actual_value', 'timestamp']
 
 # Custom Admin for UserProfile Model
 @admin.register(UserProfile)
