f7ee20ad212afce73967c5c3d53c6cda3f48c492
Plog <plog@plog.net>
2024-10-21 21:48:21 +0800

    not Oauth

--- monitor/urls.py
+++ monitor/urls.py
@@ -5,7 +5,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
-from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework.authentication import SessionAuthentication
 
 schema_view = get_schema_view(
@@ -29,7 +28,6 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0)         , name='schema-json'),
     path('swagger/'        , schema_view.with_ui('swagger', cache_timeout=0) , name='schema-swagger-ui'),
     path('redoc/'          , schema_view.with_ui('redoc', cache_timeout=0)   , name='schema-redoc'),
-    path('oauth/'          , include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/'            , include(router.urls)),
 
     # Pages
